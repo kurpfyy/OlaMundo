38a3a68b7fc032563692bee8aefb75a82485e640
kurpfy <dudubartoli@gmail.com>
2023-07-31 11:11:59 -0300

    print feito

---
+++ variavel.py
@@ -0,0 +1,12 @@
+#input é usado para perguntar algo ao usuario#
+
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco
+de
+Coments
+Permite que varias linhas se tormem um comentario
+'''
+
+print("Óla,", nome , ". Tudo bem com você")
